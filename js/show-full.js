b1eb7f1553ea01a69fbd8101185fef08b4e1eda4
Kitkat <hinhilla@list.ru>
2021-02-24 14:13:29 +0300

    Открытие большого изображения

--- js/show-full.js
+++ js/show-full.js
@@ -1,29 +1,34 @@
 
 import { pictures as currentPictures } from './show.js';
-import { randomArray as currentArray } from './show.js';
+import { randomArray as randomPhotoList } from './show.js';
 
-const bigPictureBlock = document.querySelector('.big-picture');
+const pageBody = document.body;
+const bigPictureBlock = pageBody.querySelector('.big-picture');
 const bigPictureWrapper = bigPictureBlock.querySelector('.big-picture__img');
-let bigPicture = bigPictureWrapper.querySelector('img');
+const bigPicture = bigPictureWrapper.querySelector('img');
 const bigPictureCloseButton = bigPictureBlock.querySelector('.big-picture__cancel');
 const bigPictureLike = bigPictureBlock.querySelector('.likes-count');
 const bigPictureCommentsCount = bigPictureBlock.querySelector('.comments-count');
 const bigPictureCommentsList = bigPictureBlock.querySelector('.social__comments');
 const bigPictureCommentsItem = bigPictureCommentsList.querySelector('li');
 const bigPictureDescription = bigPictureBlock.querySelector('.social__caption');
-const body = document.querySelector('body');
+
+const HIDDEN_STATE = 'hidden';
+const OPEN_MODAL_STATE = 'modal-open';
+
 
 const commentCount = bigPictureBlock.querySelector('.social__comment-count');
 const newPicturesButton = bigPictureBlock.querySelector('.comments-loader');
 const currentPicturesList = currentPictures.querySelectorAll('.picture');
+const listFragment = document.createDocumentFragment();
 
-for (let i = 0; i < currentPicturesList.length; i++) {
-  const currentPicture = currentPicturesList[i];
+currentPicturesList.forEach((_, index, collection) => {
+  const currentPicture = collection[index];
   currentPicture.addEventListener('click', () => {
-    body.classList.add('modal-open');
-    bigPictureBlock.classList.remove('hidden');
-    commentCount.classList.add('hidden');
-    newPicturesButton.classList.add('hidden');
+    pageBody.classList.add(OPEN_MODAL_STATE);
+    bigPictureBlock.classList.remove(HIDDEN_STATE);
+    commentCount.classList.add(HIDDEN_STATE);
+    newPicturesButton.classList.add(HIDDEN_STATE);
 
     const currentImg = currentPicture.querySelector('.picture__img');
     const currentLike = currentPicture.querySelector('.picture__likes');
@@ -33,37 +38,44 @@ for (let i = 0; i < currentPicturesList.length; i++) {
     bigPicture.src = currentImg.src;
     bigPictureLike.textContent = currentLike.textContent;
     bigPictureCommentsCount.textContent = currentCommentsCount.textContent;
-    bigPictureDescription.textContent = currentArray[i].description;
+    bigPictureDescription.textContent = randomPhotoList[index].description;
 
-    getAllComments(i);
+
+    getAllComments(index);
   });
-}
-
+})
 
 const getAllComments = (currentElement) => {
-  const obj = currentArray[currentElement];
-  const commentsList = obj.comments;
+  const commentsList = randomPhotoList[currentElement].comments;
   bigPictureCommentsList.innerHTML = '';
-  for (let j = 0; j < commentsList.length; j++) {
-    let currentComment = bigPictureCommentsItem.cloneNode(true);
-    let currentCommentImg = currentComment.querySelector('.social__picture');
-    let currentCommentText = currentComment.querySelector('.social__text');
-    let commentMessage = commentsList[j].message.join(' ');
-    let commentsMessageList = commentMessage;
+  commentsList.forEach((_, index, array) => {
+    const currentComment = bigPictureCommentsItem.cloneNode(true);
+    const currentCommentImg = currentComment.querySelector('.social__picture');
+    const currentCommentText = currentComment.querySelector('.social__text');
+    const commentMessage = array[index].message.join(' ');
 
-    currentCommentImg.src = commentsList[j].avatar;
-    currentCommentImg.alt = commentsList[j].name;
-    currentCommentText.textContent = commentsMessageList;
+    currentCommentImg.src = array[index].avatar;
+    currentCommentImg.alt = array[index].name;
+    currentCommentText.textContent = commentMessage;
 
-    let element = bigPictureCommentsList.appendChild(currentComment);
-    bigPictureCommentsList.innerHTML += element;
-  }
+    listFragment.appendChild(currentComment);
+
+  });
+  bigPictureCommentsList.appendChild(listFragment);
 }
 
+
 bigPictureCloseButton.addEventListener('click', () => {
-  bigPictureBlock.classList.add('hidden');
-  body.classList.remove('modal-open');
+  bigPictureBlock.classList.add(HIDDEN_STATE);
+  pageBody.classList.remove(OPEN_MODAL_STATE);
 });
+document.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27) {
+    bigPictureBlock.classList.add(HIDDEN_STATE);
+    pageBody.classList.remove(OPEN_MODAL_STATE);
+  }
+})
+
 
 
 
