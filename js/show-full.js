e0ea6faf493ec7170372ae37491a8fbe488ad743
Kitkat <hinhilla@list.ru>
2021-02-25 13:26:54 +0300

    Открытие большого изображения

--- js/show-full.js
+++ js/show-full.js
@@ -2,6 +2,9 @@
 import { pictures as currentPictures } from './show.js';
 import { randomArray as randomPhotoList } from './show.js';
 
+const HIDDEN_STATE = 'hidden';
+const OPEN_MODAL_STATE = 'modal-open';
+
 const pageBody = document.body;
 const bigPictureBlock = pageBody.querySelector('.big-picture');
 const bigPictureWrapper = bigPictureBlock.querySelector('.big-picture__img');
@@ -12,18 +15,12 @@ const bigPictureCommentsCount = bigPictureBlock.querySelector('.comments-count')
 const bigPictureCommentsList = bigPictureBlock.querySelector('.social__comments');
 const bigPictureCommentsItem = bigPictureCommentsList.querySelector('li');
 const bigPictureDescription = bigPictureBlock.querySelector('.social__caption');
-
-const HIDDEN_STATE = 'hidden';
-const OPEN_MODAL_STATE = 'modal-open';
-
-
 const commentCount = bigPictureBlock.querySelector('.social__comment-count');
 const newPicturesButton = bigPictureBlock.querySelector('.comments-loader');
 const currentPicturesList = currentPictures.querySelectorAll('.picture');
 const listFragment = document.createDocumentFragment();
 
-currentPicturesList.forEach((_, index, collection) => {
-  const currentPicture = collection[index];
+currentPicturesList.forEach((currentPicture, index) => {
   currentPicture.addEventListener('click', () => {
     pageBody.classList.add(OPEN_MODAL_STATE);
     bigPictureBlock.classList.remove(HIDDEN_STATE);
@@ -40,7 +37,6 @@ currentPicturesList.forEach((_, index, collection) => {
     bigPictureCommentsCount.textContent = currentCommentsCount.textContent;
     bigPictureDescription.textContent = randomPhotoList[index].description;
 
-
     getAllComments(index);
   });
 })
@@ -48,18 +44,17 @@ currentPicturesList.forEach((_, index, collection) => {
 const getAllComments = (currentElement) => {
   const commentsList = randomPhotoList[currentElement].comments;
   bigPictureCommentsList.innerHTML = '';
-  commentsList.forEach((_, index, array) => {
+  commentsList.forEach((element) => {
     const currentComment = bigPictureCommentsItem.cloneNode(true);
     const currentCommentImg = currentComment.querySelector('.social__picture');
     const currentCommentText = currentComment.querySelector('.social__text');
-    const commentMessage = array[index].message.join(' ');
+    const commentMessage = element.message.join(' ');
 
-    currentCommentImg.src = array[index].avatar;
-    currentCommentImg.alt = array[index].name;
+    currentCommentImg.src = element.avatar;
+    currentCommentImg.alt = element.name;
     currentCommentText.textContent = commentMessage;
 
     listFragment.appendChild(currentComment);
-
   });
   bigPictureCommentsList.appendChild(listFragment);
 }
